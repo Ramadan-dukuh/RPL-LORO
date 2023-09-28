bfeb89f3a5795c2d33999f6dbffb62fa1b8d5a38
Satria Jati <108228328+Prinz-zen@users.noreply.github.com>
2023-09-28 11:30:08 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,3 +15,33 @@ var swiper = new Swiper(".slide-content", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+function scrollToElement(elementSelector, instance = 0) {
+  const elements = document.querySelectorAll(elementSelector);
+  // Check if there are elements matching the selector and if the requested instance exists
+  if (elements.length > instance) {
+      // Scroll to the specified instance of the element
+      elements[instance].scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+
+link1.addEventListener('click', () => {
+  scrollToElement('nav');
+});
+
+link2.addEventListener('click', () => {
+  scrollToElement('.header',);
+});
+
+link3.addEventListener('click', () => {
+  scrollToElement('.header', 1);
+});
+
+link4.addEventListener('click', () => {
+  scrollToElement('.f-class');
+});
